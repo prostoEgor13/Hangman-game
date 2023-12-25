c6cae49a8c5f4626bce791800dca25b43661719a
Egor <egor.zhirkov00@mail.ru>
2023-12-25 15:35:32 +0300

    change checkbox and add word

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,2 +1,2 @@
-export const WORDS = ['code', 'frontend', 'programming']
+export const WORDS = ['code', 'frontend', 'programming', 'polins']
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
